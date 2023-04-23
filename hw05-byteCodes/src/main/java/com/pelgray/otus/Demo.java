61e6fc367aa961c26a15b3f99a9fd9ade4f989b4
Polina Borisova <BorisovaPauline@gmail.com>
2023-04-23 16:06:09 +0300

    hw #5 Добавлено кэширование результатов рефлексии

--- hw05-byteCodes/src/main/java/com/pelgray/otus/Demo.java
+++ hw05-byteCodes/src/main/java/com/pelgray/otus/Demo.java
@@ -4,16 +4,31 @@ import com.pelgray.otus.logging.test.TestLoggingInterface;
 
 public class Demo {
     public void action(TestLoggingInterface testLogging) {
+        int i = 0;
+        i = callAllMethods(testLogging, i);
+
+        System.out.println("Calls for the second time");
+        i = callAllMethods(testLogging, i);
+
+        System.out.println("Calls for the third time");
+        i = callAllMethods(testLogging, i);
+
+        System.out.println("Calls for the fourth time");
+        callAllMethods(testLogging, i);
+    }
+
+    private static int callAllMethods(TestLoggingInterface testLogging, int i) {
         testLogging.calculation();
-        testLogging.calculation(1);
-        testLogging.calculation(2, 3);
-        testLogging.calculation(4, 5, "6");
-        System.out.println("Got result = " + testLogging.calculation(7, 8, 9));
+        testLogging.calculation(i++);
+        testLogging.calculation(i++, i++);
+        testLogging.calculation(i++, i++, String.valueOf(i++));
+        System.out.println("Got result = " + testLogging.calculation(i++, i++, i++));
 
         testLogging.calculationNoLog();
-        testLogging.calculationNoLog(7);
-        testLogging.calculationNoLog(8, 9);
-        testLogging.calculationNoLog(10, 11, "12");
-        System.out.println("Got result without log = " + testLogging.calculationNoLog(13, 14, 15));
+        testLogging.calculationNoLog(i++);
+        testLogging.calculationNoLog(i++, i++);
+        testLogging.calculationNoLog(i++, i++, String.valueOf(i++));
+        System.out.println("Got result without log = " + testLogging.calculationNoLog(i++, i++, i++));
+        return i;
     }
 }
