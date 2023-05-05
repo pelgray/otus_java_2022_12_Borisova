2704ace8f891768aba8d3f75ed1230a15f898e0e
Polina Borisova <BorisovaPauline@gmail.com>
2023-05-05 23:00:34 +0300

    hw #7 Обработчик сообщений. Исправления

--- hw07-structuralPatterns/src/main/java/ru/otus/model/Message.java
+++ hw07-structuralPatterns/src/main/java/ru/otus/model/Message.java
@@ -125,7 +125,7 @@ public class Message {
 
     public Builder toBuilder() {
         return new Builder(id, field1, field2, field3, field4, field5, field6, field7, field8, field9, field10,
-                           field11, field12, field13);
+                           field11, field12, field13 != null ? new ObjectForMessage(field13) : null);
     }
 
     @Override
@@ -197,7 +197,7 @@ public class Message {
             this.field10 = field10;
             this.field11 = field11;
             this.field12 = field12;
-            this.field13 = field13 != null ? new ObjectForMessage(field13) : null;
+            this.field13 = field13;
         }
 
         public Builder field1(String field1) {
