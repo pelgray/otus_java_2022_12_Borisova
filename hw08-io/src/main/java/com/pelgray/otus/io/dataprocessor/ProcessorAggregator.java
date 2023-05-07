f861c430df9a6a0e56374fcfc06f06f1cf9762c0
Polina Borisova <BorisovaPauline@gmail.com>
2023-05-07 13:25:31 +0300

    hw #8 Реализация домашнего задания в двух вариантах: с использованием Gson и Jackson

--- hw08-io/src/main/java/com/pelgray/otus/io/dataprocessor/ProcessorAggregator.java
+++ hw08-io/src/main/java/com/pelgray/otus/io/dataprocessor/ProcessorAggregator.java
@@ -4,12 +4,21 @@ import com.pelgray.otus.io.model.Measurement;
 
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
 
 public class ProcessorAggregator implements Processor {
-
+    /**
+     * группирует выходящий список по name, при этом суммирует поля value
+     */
     @Override
     public Map<String, Double> process(List<Measurement> data) {
-        //группирует выходящий список по name, при этом суммирует поля value
-        return null;
+        return data.stream()
+                .collect(Collectors.toMap(
+                        Measurement::getName,
+                        Measurement::getValue,
+                        Double::sum,
+                        TreeMap::new
+                ));
     }
 }
--- hw08-io/src/main/java/com/pelgray/otus/io/dataprocessor/jackson/FileSerializer.java
+++ hw08-io/src/main/java/com/pelgray/otus/io/dataprocessor/jackson/FileSerializer.java
@@ -1,16 +1,39 @@
 package com.pelgray.otus.io.dataprocessor.jackson;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.pelgray.otus.io.dataprocessor.FileProcessException;
 import com.pelgray.otus.io.dataprocessor.Serializer;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.Map;
 
 public class FileSerializer implements Serializer {
+    private final String fileName;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     public FileSerializer(String fileName) {
+        this.fileName = fileName;
     }
 
+    /**
+     * формирует результирующий json и сохраняет его в файл
+     */
+    @SuppressWarnings("ResultOfMethodCallIgnored")
     @Override
     public void serialize(Map<String, Double> data) {
-        //формирует результирующий json и сохраняет его в файл
+        if (fileName == null || fileName.isBlank()) {
+            throw new FileProcessException("The output file must not be null or blank");
+        }
+        var outputFile = new File(fileName);
+        try {
+            Files.deleteIfExists(outputFile.toPath());
+            outputFile.createNewFile();
+            objectMapper.writeValue(outputFile, data);
+        } catch (IOException e) {
+            throw new FileProcessException(e);
+        }
     }
 }
